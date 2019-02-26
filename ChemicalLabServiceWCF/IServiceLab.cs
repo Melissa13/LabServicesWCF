d37833a11f579921dd5f732db36705bf71c87ee3
Melisa <melissalantigua@hotmail.com>
2019-02-26 12:06:54 -0400

    first commit

---
+++ ChemicalLabServiceWCF/IServiceLab.cs
@@ -0,0 +1,27 @@
+﻿using LabServerConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ChemicalLabServiceWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceLab
+    {
+
+        [OperationContract]
+        //string GetData(int value);
+        bool RegistrarMatricula(int matricula, string password);
+
+        [OperationContract]
+        List<Estudiantes> ObtenerEstudiantes(int idEstudiantes);
+
+        // TODO: Add your service operations here
+    }
+
+}
---
+++ ChemicalLabServiceWCF/ServiceLab.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using LabServerConnection;
+
+namespace ChemicalLabServiceWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceLab : IServiceLab
+    {
+        DBCHEMICALLABEntities conexionDB = new DBCHEMICALLABEntities();
+
+        public List<Estudiantes> ObtenerEstudiantes(int idEstudiantes)
+        {
+            return conexionDB.Estudiantes.Where(Estudiantes => Estudiantes.EstudianteID == idEstudiantes || idEstudiantes == -1).ToList();
+        }
+
+        public bool RegistrarMatricula(int matricula, string password)
+        {
+            try
+            {
+                var nuevoEstudiante = conexionDB.Estudiantes.Create();
+                //if para verificar aqui
+                nuevoEstudiante.EstudianteID = matricula;
+                nuevoEstudiante.EstPassword = password;
+
+
+                conexionDB.Estudiantes.Add(nuevoEstudiante);
+                //guardar en la base de datos
+                conexionDB.SaveChanges();
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
